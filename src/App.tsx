12bc63c0c9cf5763c940ebce170a071e713c6575
Mikhail Morozov <inferst@gmail.com>
2024-02-07 17:51:47 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { createSignal } from 'solid-js'
+import './App.css'
+
+function App() {
+  const [count, setCount] = createSignal(0)
+
+  return (
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
